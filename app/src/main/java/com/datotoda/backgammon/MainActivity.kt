d3d409c12a0420004a0d522ea548fec666e7ff05
saba_guledani <115211537+SabaGuledani@users.noreply.github.com>
2025-01-05 14:55:58 +0100

    added model

--- app/src/main/java/com/datotoda/backgammon/MainActivity.kt
+++ app/src/main/java/com/datotoda/backgammon/MainActivity.kt
@@ -2,7 +2,14 @@ package com.datotoda.backgammon
 
 import android.os.Bundle
 import android.widget.RelativeLayout
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.datotoda.backgammon.ml.FirstTfModel
+import com.datotoda.backgammon.ml.FirstTfModel2
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
 
 
 class MainActivity : AppCompatActivity() {
@@ -20,5 +27,31 @@ class MainActivity : AppCompatActivity() {
         // its view to our relative layout.
         val paintView = PaintView(this)
         relativeLayout?.addView(paintView)
+
+
+        val model = FirstTfModel2.newInstance(applicationContext)
+
+
+
+        val inputData = floatArrayOf(0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f)
+
+        val byteBuffer = ByteBuffer.allocateDirect(4 * 198).order(ByteOrder.nativeOrder())
+        inputData.forEach { byteBuffer.putFloat(it) }
+        byteBuffer.rewind()
+
+        val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 198), DataType.FLOAT32)
+        inputFeature0.loadBuffer(byteBuffer)
+
+        // Runs model inference and gets result.
+        val outputs = model.process(inputFeature0)
+        val outputFeature0 = outputs.outputFeature0AsTensorBuffer
+
+        println("Raw Output: ${outputFeature0.floatArray.joinToString(", ")}")
+        // Process the output tensor (e.g., print results)
+        val outputArray = outputs.outputFeature0AsTensorBuffer.floatArray
+        val textt = "Model Output: ${outputArray.joinToString(", ")}"
+        Toast.makeText(this, textt, Toast.LENGTH_SHORT).show()
+        // Releases model resources if no longer used.
+        model.close()
     }
 }
